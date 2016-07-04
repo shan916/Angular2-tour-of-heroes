4a45eff093b64e5b76e1a7545c5eab13eb7ad22e
Zeeshan <shan916@icloud.com>
2016-07-03 23:55:46 -0700

    Completed Ch6 - Routing

---
+++ app/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component'
+import { HeroService } from './hero.service';
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl: 'app/heroes.component.html',
+    styleUrls: ['app/heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+    heroes: Hero[];
+	selectedHero: Hero;
+
+    constructor(private heroService: HeroService, private router: Router) { }
+
+    getHeroes() {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    onSelect(hero: Hero) { 
+        this.selectedHero = hero; 
+    }
+
+    gotoDetail() {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+}
